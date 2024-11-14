e04bfda957d4547c598ce5f361aeccc274037a60
piyumaldk <piyumaldk@gmail.com>
2024-11-14 16:30:44 +0530

    Add run scripts and build workflow

--- config.js
+++ config.js
@@ -3,11 +3,12 @@ var config = {};
 config.apiMetaDataAPI = "http://localhost:9090/apiMetadata/"
 config.devportalAPI = "http://localhost:3000/devportal/"
 config.port = 3000
-config.mode = 'multi'
+config.pathToContent = "./src/"
+config.mode = 'development'
 config.db = {
     username: 'postgres',
     password: 'postgres',
-    database: 'dev',
+    database: 'DEVPORTAL_NEWSCHEMA',
     host: 'localhost',
     dialect: 'postgres',
 };
